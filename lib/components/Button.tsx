81c7c955d4b354eb923b9ff1edcb1b149cedf96f
kyu <2066apple@naver.com>
2024-04-16 17:53:17 +0900

    ✨ 테스트 버튼 추가

--- lib/components/Button.tsx
+++ lib/components/Button.tsx
@@ -1,5 +1,12 @@
-export const Button = () => {
+interface ButtonProps {
+  children: React.ReactNode;
+}
+
+export const Button = (props: ButtonProps) => {
+  const { children, ...rest } = props;
   return (
-    <div className="w-[100px] h-[50px] rounded bg-black text-white">Button</div>
+    <div className="w-[100px] h-[50px] rounded bg-black text-white" {...rest}>
+      {children}
+    </div>
   );
 };
