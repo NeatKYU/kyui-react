4a09c8fe440097cfba8855ff866657067c68c016
kyu <2066apple@naver.com>
2024-04-16 11:39:46 +0900

    🎉 기본세팅

---
+++ lib/components/Button.tsx
@@ -0,0 +1,5 @@
+export const Button = () => {
+  return (
+    <div className="w-[100px] h-[50px] rounded bg-black text-white">Button</div>
+  );
+};
---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+import "./index.css";
+import { Button } from "./components/Button";
+
+export { Button };
