b27bd77d1789a1e59f936d615a20473ca3e65e0c
kyu <2066apple@naver.com>
2024-05-01 16:51:07 +0900

    🚧 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,40 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
-import tailwindcss from "tailwindcss";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "./lib/index.ts"),
-      name: "react-beautiful-timeline",
-      fileName: (format) => `index.${format}.js`,
+    resolve: {
+        alias: [
+            { find: '@/lib', replacement: resolve(__dirname, './lib') },
+            { find: '@/utils', replacement: resolve(__dirname, './utils') },
+        ],
     },
-    rollupOptions: {
-      external: ["react", "react-dom", "tailwindcss"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          tailwindcss: "tailwindcss",
+    build: {
+        lib: {
+            entry: resolve(__dirname, './lib/index.ts'),
+            name: 'react-beautiful-timeline',
+            fileName: (format) => `index.${format}.js`,
         },
-      },
+        rollupOptions: {
+            external: ['react', 'react-dom', 'tailwindcss'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                    tailwindcss: 'tailwindcss',
+                },
+            },
+        },
+        sourcemap: true,
+        emptyOutDir: true,
     },
-    sourcemap: true,
-    emptyOutDir: true,
-  },
-  plugins: [react(), dts({ rollupTypes: true })],
-  css: {
-    postcss: {
-      plugins: [tailwindcss],
+    plugins: [react(), dts({ rollupTypes: true })],
+    css: {
+        postcss: {
+            plugins: [tailwindcss],
+        },
     },
-  },
 });
