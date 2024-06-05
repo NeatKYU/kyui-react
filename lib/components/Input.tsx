8139091200fe10e8eed2f2abda5d83114600b45c
kyu <2066apple@naver.com>
2024-06-05 13:37:21 +0900

    💄 input 스타일 변경

--- lib/components/Input.tsx
+++ lib/components/Input.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from 'react'
+import clsx from 'clsx'
+
+const InputStyles = 'bg-white border px-2 py-1 focus:outline-none focus:ring-2 focus:ring-zinc-700 transition-all'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-    const { type, className, ...rest } = props;
-    return <input ref={ref} type={type} className={clsx('border', className)} {...rest} />;
-});
+    const { type, className, ...rest } = props
+    return <input ref={ref} type={type} className={clsx(InputStyles, className)} {...rest} />
+})
 
-Input.displayName = 'Input';
+Input.displayName = 'Input'
 
-export { Input };
+export { Input }
