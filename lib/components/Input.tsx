2a4363c2ca3af2fd10423c8562589d302cd95382
kyu <2066apple@naver.com>
2024-06-12 13:15:43 +0900

    ✨ file input 추가

--- lib/components/Input.tsx
+++ lib/components/Input.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import clsx from 'clsx'
 
-const InputStyles = 'bg-white border px-2 py-1 focus:outline-none focus:ring-2 focus:ring-zinc-700 transition-all'
+const InputStyles =
+    'bg-white border px-3 py-1 focus:outline-none focus:ring-2 focus:ring-zinc-700 transition-all placeholder:text-gray-400 cursor-pointer'
+const FileStyles =
+    'bg-white border focus:outline-none focus:ring-2 focus:ring-zinc-700 transition-all placeholder:text-gray-400 cursor-pointer file:border-0 file:rounded-none file:px-3 file:py-2 file:mr-3'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
     const { type, className, ...rest } = props
-    return <input ref={ref} type={type} className={clsx(InputStyles, className)} {...rest} />
+    return (
+        <input
+            aria-label="file"
+            ref={ref}
+            type={type}
+            className={clsx(type === 'file' ? FileStyles : InputStyles, className)}
+            {...rest}
+        />
+    )
 })
 
 Input.displayName = 'Input'
