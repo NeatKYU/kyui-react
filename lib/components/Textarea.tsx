c429fa19c9e8bad70a923a46e576540d47b8ad13
kyu <2066apple@naver.com>
2024-06-12 13:21:30 +0900

    💄 textarea 스타일 변경

--- lib/components/Textarea.tsx
+++ lib/components/Textarea.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import clsx from 'clsx'
 
+const TextareaStyles = 'border focus:outline-none focus:ring-2 focus:ring-zinc-700 transition-all px-3 py-1'
+
 interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>((props, ref) => {
     const { className, ...rest } = props
-    return <textarea ref={ref} className={clsx('border', className)} {...rest} />
+    return <textarea ref={ref} className={clsx(TextareaStyles, className)} {...rest} />
 })
 
 Textarea.displayName = 'Textarea'
