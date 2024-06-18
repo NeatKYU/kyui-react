16b9cf596fc9a2c0109e3d21c43e0014f9850dfb
kyu <2066apple@naver.com>
2024-06-18 15:34:09 +0900

    🔧 절대경로 설정 및 svg설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import dts from 'vite-plugin-dts';
-import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+import tailwindcss from 'tailwindcss'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
         alias: [
             { find: '@/lib', replacement: resolve(__dirname, './lib') },
             { find: '@/utils', replacement: resolve(__dirname, './utils') },
+            { find: '@/public', replacement: resolve(__dirname, './public') },
         ],
     },
     build: {
@@ -31,10 +33,10 @@ export default defineConfig({
         sourcemap: true,
         emptyOutDir: true,
     },
-    plugins: [react(), dts({ rollupTypes: true })],
+    plugins: [react(), dts({ rollupTypes: true }), svgr()],
     css: {
         postcss: {
             plugins: [tailwindcss],
         },
     },
-});
+})
