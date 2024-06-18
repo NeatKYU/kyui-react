e151611941874e824e5e54f3af8dc7a68151cdf8
kyu <2066apple@naver.com>
2024-06-18 15:35:09 +0900

    ✅ 인덱스 업데이트

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,7 @@ import './index.css'
 import { Button } from './components/Button'
 import { Input } from './components/Input'
 import { Textarea } from './components/Textarea'
+import { Avator } from './components/Avator'
+import { Select } from './components/Select'
 
-export { Button, Input, Textarea }
+export { Button, Input, Textarea, Avator, Select }
